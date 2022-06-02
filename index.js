5a1e5ac718fd0ce18cba3f8ca1bb763ba3d722cd
MoisesMed <br.moises@hotmail.com>
2022-06-02 10:12:31 -0300

    ajustes cors

--- index.js
+++ index.js
@@ -4,10 +4,11 @@ const express = require('express')
 const authRoutes = require('./routes/authRoutes')
 const Middleauth = require('./middlewares/authentication')
 const app = express()
+const cors = require('cors')
 
-// app.use(cors())
+app.use(cors())
 app.use(express.json())
-// app.use(express.urlencoded({extended: true, }),)
+app.use(express.urlencoded({extended: true, }),)
 
 
 // Models
