6d5fd0473b85eb5d699ae4522c0ee972814be0bb
MoisesMed <br.moises@hotmail.com>
2022-06-09 09:52:11 -0300

    task routes

--- models/Task.js
+++ models/Task.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose')
 const Task = mongoose.model('Task',{
     id_user:String,
     title:String,
-    status: Number,
+    status: {type: Number,enum:[1,2,3]},
     description: String,
     due_date: String
 })
--- routes/taskRoutes.js
+++ routes/taskRoutes.js
@@ -1,44 +1,43 @@
-const router = require('express').Router()
-const Task = require('../models/Task')
+const router = require("express").Router();
+const Task = require("../models/Task");
 const MiddleAuth = require("../middlewares/authentication");
-router.use(MiddleAuth)
+router.use(MiddleAuth);
 
-router.post('/create', async (req, res) => {
-    const {
-        title,
-        status,
-        description,
-        due_date
-    } = req.body
+router.post("/create", async (req, res) => {
+  const { title, status, description, due_date } = req.body;
 
-    if (!title) {
-        res.status(422).json({ error: "Título é obrigatório" })
-        return
-    }
+  if (!title) {
+    res.status(422).json({ error: "Título é obrigatório" });
+    return;
+  }
 
-    if (!status) {
-        res.status(422).json({ error: "Escolha o status da tarefa !" })
-        return
-    }
-    
-    const task = { id_user: req.user_id, title, status, description, due_date }
+  if(Task.find({title : title})){
+    res.status(422).json({ error: "Essa tarefa já existe!" });
+  }
 
-    try {
-        await Task.create(task)
-        res.status(201).json({ message: "Tarefa criada com sucesso !!!" })
-    } catch (error) {
-        res.status(500).json({ error: error })
-    }
+  const task = {
+    id_user: req.user_id,
+    title,
+    status: status ? status : 1,
+    description,
+    due_date,
+  };
 
-})
+  try {
+    await Task.create(task);
+    res.status(201).json({ message: "Tarefa criada com sucesso !!!" });
+  } catch (error) {
+    res.status(500).json({ error: error });
+  }
+});
 
-router.get('/', async (req, res) => {
-    try {
-        const tasks = await Task.find({id_user:req.user_id})
-        res.status(200).json(tasks)
-    } catch (error) {
-        res.status(500).json({error: error})
-    }
-})
+router.get("/", async (req, res) => {
+  try {
+    const tasks = await Task.find({ id_user: req.user_id });
+    res.status(200).json(tasks);
+  } catch (error) {
+    res.status(500).json({ error: error });
+  }
+});
 
-module.exports = router
+module.exports = router;
